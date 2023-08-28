8393b698112d8b8856e21da995a8db5081e95ace
alanrosenberg77 <alanrosenberg77@gmail.com>
2023-08-28 15:58:16 -0500

    created agent interface
    just a stub with description comment

---
+++ src/main/java/cs440/agent/Agent.java
@@ -0,0 +1,15 @@
+package cs440.agent;
+
+/**
+ * An instance of this interface will be capable of discerning the information
+ * about the agent's environment gathered by sensors. It will also be capable
+ * of interacting with the environment using its actuators. The agents will
+ * make decisions based on the state of its environment and the condition-action
+ * rules given to it.
+ * 
+ * @author alanr
+ *
+ */
+public interface Agent {
+
+}
